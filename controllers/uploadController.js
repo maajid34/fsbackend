893a03f600a23332339160870c34676f120b334f
abdi majiid haji <najacade23456@gmail.com>
2026-05-23 17:40:23 +0300

    chnaged

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,3 +1,8 @@
+
+
+
+// import Evidence from "../models/Evidence.js";
+
 // export const uploadEvidenceFile = async (req, res) => {
 //   try {
 //     if (!req.file) {
@@ -8,15 +13,19 @@
 
 //     const fileUrl = `${process.env.R2_PUBLIC_URL}/${req.file.key}`;
 
+//     // save into mongodb
+//     const evidence = await Evidence.create({
+//       original_name: req.file.originalname,
+//       file_name: req.file.key,
+//       file_url: fileUrl,
+//       mime_type: req.file.mimetype,
+//       size: req.file.size,
+//       uploaded_by: req.user?._id || null,
+//     });
+
 //     res.status(201).json({
 //       message: "File uploaded successfully",
-//       file: {
-//         original_name: req.file.originalname,
-//         file_name: req.file.key,
-//         file_url: fileUrl,
-//         mime_type: req.file.mimetype,
-//         size: req.file.size,
-//       },
+//       file: evidence,
 //     });
 //   } catch (error) {
 //     res.status(500).json({
@@ -24,6 +33,7 @@
 //     });
 //   }
 // };
+
 
 import Evidence from "../models/Evidence.js";
 
@@ -37,13 +47,14 @@ export const uploadEvidenceFile = async (req, res) => {
 
     const fileUrl = `${process.env.R2_PUBLIC_URL}/${req.file.key}`;
 
-    // save into mongodb
     const evidence = await Evidence.create({
       original_name: req.file.originalname,
       file_name: req.file.key,
       file_url: fileUrl,
       mime_type: req.file.mimetype,
       size: req.file.size,
+      module: req.body.module || "general",
+      related_id: req.body.related_id || null,
       uploaded_by: req.user?._id || null,
     });
 
